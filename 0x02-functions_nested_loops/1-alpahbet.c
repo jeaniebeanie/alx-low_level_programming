f0702b02e40fd4ab842298b01ab336983ec789b0
jeaniebeanie <jeanettenyatoro@gmail.com>
2022-03-15 23:03:33 +0300

    printout alphabet in lowercase then new line

---
+++ 0x02-functions_nested_loops/1-alpahbet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** main - Entry point 
+ *
+ *
+ * Return- Always (0) (Success)
+ */
+
+int main(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch ++);
+	_putchar(ch);
+	_putchar(\n);
+
+	return 0;
+}
