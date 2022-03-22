85c8a7a80b91f45f8d9fae43aecb26850c740e5d
jeaniebeanie <99317595+jeaniebeanie@users.noreply.github.com>
2022-03-22 04:19:02 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
   #ifndef main 
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,7 +10,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _putchar(char *str);
+int _atoi(char *s);
 
 
 #define main 
